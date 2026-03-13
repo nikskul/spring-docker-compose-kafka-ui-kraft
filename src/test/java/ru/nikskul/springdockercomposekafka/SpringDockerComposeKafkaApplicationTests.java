236a36e85b303f47f3b0d6ecfe6e86db361be33e
nikskul <nikitaskul2012@gmail.com>
2026-03-13 18:29:44 +0500

    Init

---
+++ src/test/java/ru/nikskul/springdockercomposekafka/SpringDockerComposeKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nikskul.springdockercomposekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerComposeKafkaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
